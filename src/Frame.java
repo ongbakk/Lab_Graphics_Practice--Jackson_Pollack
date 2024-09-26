c39f725ea8be9fa628aeb9106d387b3741c1310f
ongbakk <grayson.clark@allsaintshigh.org>
2024-09-26 10:37:57 -0500

    Initial display of smiley face.

--- src/Frame.java
+++ src/Frame.java
@@ -1,5 +1,25 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import javax.swing.*;
+
+public class Frame extends JFrame
+{
+    private static final int WIDTH = 800;
+    private static final int HEIGHT = 600;
+
+    public Frame()
+    {
+        super("Smiley Face");
+        setSize(WIDTH,HEIGHT);
+
+        getContentPane().add(new SmileyFace());
+
+
+        setVisible(true);
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main( String args[] )
+    {
+        Frame run = new Frame();
     }
 }
--- src/SmileyFace.java
+++ src/SmileyFace.java
@@ -1,4 +1,28 @@
-package PACKAGE_NAME;
+import java.awt.Graphics;
+import java.awt.Color;
+import java.awt.Canvas;
 
-public class SmileyFace {
+
+public class SmileyFace extends Canvas
+{
+    public SmileyFace()
+    {
+        setBackground(Color.WHITE);
+    }
+
+    public void paint(Graphics window)
+    {
+        window.setColor(Color.pink);
+        window.fillOval(200,100,400,400);
+
+        window.setColor(Color.WHITE);
+        window.fillOval(250,200,100,100);
+        window.fillOval(455,200,100,100);
+
+        window.setColor(Color.BLACK);
+        window.drawOval(200,100,400,400);
+        window.fillOval(275,230,50,25);
+        window.fillOval(480,230,50,25);
+        window.fillArc(350,400,100,50,0,-180);
+    }
 }
